aff3d17f4f368e0a997383c48da90075833835ed
Muhammed ikinci <muhammedikinci@outlook.com>
2021-04-30 21:37:17 +0300

    clear settings

--- src/Persistence/MongoDBSettings.cs
+++ src/Persistence/MongoDBSettings.cs
@@ -4,9 +4,6 @@ namespace Persistence
 {
     public class MongoDBSettings : IDatabaseSettings
     {
-        public string PartnerCollectionName { get; set; }
-        public string OrderCollectionName { get; set; }
-        public string ProductCollectionName { get; set; }
         public string ConnectionString { get; set; }
         public string DatabaseName { get; set; }
     }
