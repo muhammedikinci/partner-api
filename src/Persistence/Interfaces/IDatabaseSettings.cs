c97ebdeb6edd5efe3ccb2bc38a6c4bcf0ee1a4f6
Muhammed ikinci <muhammedikinci@outlook.com>
2021-04-30 21:37:55 +0300

    clear interface

--- src/Persistence/Interfaces/IDatabaseSettings.cs
+++ src/Persistence/Interfaces/IDatabaseSettings.cs
@@ -2,9 +2,6 @@ namespace Persistence.Interfaces
 {
     public interface IDatabaseSettings
     {
-        string PartnerCollectionName { get; set; }
-        string OrderCollectionName { get; set; }
-        string ProductCollectionName { get; set; }
         string ConnectionString { get; set; }
         string DatabaseName { get; set; }
     }
